4267102025256eb315d6bb79680ad9de8a39286c
BBennettChiba <BrysonCBennett@gmail.com>
2022-05-13 14:20:53 +0900

    added args from command line

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,35 @@ mod draw;
 mod game;
 mod snake;
 use draw::to_coord_u32;
+use game::Game;
 use piston_window::types::Color;
 use piston_window::*;
-
-use game::Game;
+use std::env;
 
 const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
 fn main() {
-    let (width, height) = (25, 25);
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        0 | 1 | 2 => panic!("Not enough args"),
+        _ => (),
+    };
+    println!("{:?}", args);
+    let width = match args[1].parse::<i32>() {
+        Ok(num) => num,
+        Err(e) => panic!("{}", e),
+    };
+    let height = match args[2].parse::<i32>() {
+        Ok(num) => num,
+        Err(e) => panic!("{}", e),
+    };
+    if height < 10 || width < 10 {
+        panic!("dimensions are too small")
+    }
+    start_game((width, height))
+}
+
+fn start_game(dimensions: (i32, i32)) {
+    let (width, height) = dimensions;
     let mut window: PistonWindow =
         WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
             .exit_on_esc(true)
